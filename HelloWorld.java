f4a92a928b7de0f9978bd1ef665df2187320e894
akkash1134 <akkashak1418@gmail.com>
2025-11-29 19:40:30 +0530

    Add second print line for feature update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Git! This is my first Java program in this repo.");
+	System.out.println("This is my new feature update!");
     }
 }
